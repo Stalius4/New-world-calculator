ee26c4e27228bbf544db7e95769005f5a62051dc
Stalius4 <deivydasrimgaila@gmail.com>
2022-10-29 13:19:16 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useState} from "react";
+import Consumable from './Items/Consumable/consumable';
 
 function App() {
+
+const [consumableList, setConsumableList] = useState ([])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<Consumable consumableList={consumableList} setConsumableList={setConsumableList}></Consumable>
     </div>
   );
 }
---
+++ src/Items/Consumable/consumable.js
@@ -0,0 +1,106 @@
+import React, { useEffect } from "react";
+import "./consumable.css"
+
+const Consumable= ({consumableList, setConsumableList})=>{
+
+
+
+
+    const fetchData = async () => {
+        try {
+    
+            const offsetArr = [1, 2];
+            const itemArr = [];
+    offsetArr.forEach(async(item,index) =>{
+
+
+  
+          const response = await fetch(`https://nwdb.info/db/items/consumables/raw-food/page/${item}.json`);
+          const data = await response.json();
+          data.data.forEach(async(item, index) => {
+
+            const eventObj = {
+                id: item.id,
+                tier: item.tier,
+               name: item.name,
+               icon: item.icon,
+               itemClass: item.itemClass,
+                description: item.description,  
+                rarity: item.rarity
+            };
+
+            itemArr.push(eventObj)})
+            setConsumableList(itemArr);
+          
+          if (!response.ok) {
+            throw new Error(response.statusText);
+          }
+
+
+    console.log(itemArr,"daa")
+        
+        //   console.log(consumableList, "pirmas");
+        })
+        } catch (error) {}
+      };
+      useEffect(() => {
+        fetchData();
+        
+        // eslint-disable-next-line
+      }, []);
+
+      const sortArr = () =>{
+        
+     consumableList.sort((a, b) => a.tier - b.tier)
+      }
+
+
+
+
+      return (
+        <div className="main-box">
+            <button onClick={ sortArr }>Sort by tear</button>
+       {consumableList.map((item, index) => {
+       
+        return (
+          <div key={index} className="item-box">
+
+            <img className={item.rarity === 0 ? "item-image-rarity-0":
+           item.rarity === 1 ? "item-image-rarity-1": 
+           item.rarity === 2 ? "item-image-rarity-2": "item-image-rarity-0"}
+              src={`https://cdn.nwdb.info/db/images/live/v17/icons/items/consumable/${
+                item.id ==="juniperberryt5" ? "blueberryt1":
+                item.id ==="meatsquidt1" ? "squidmeatt1":
+                item.id ==="redmeatt1" ? "meatt1":
+                item.id ==="fisht2" ? "fisht1":
+                item.id ==="fisht3" ? "fisht1":
+                item.id ==="fisht4" ? "fisht1":
+                item.id ==="enrichedredmeatt5" ? "highnutritionmeatst5":
+                item.id === "enrichedmeatportiont5" ? "highnutritionmeatst3":
+                item.id === "armadillomeatt5" ? "armadillomeat":
+                item.id === "scorpionmeatt5" ?"scorpionmeat"
+                :item.id }.png` }
+              alt="sd"
+              onError={e => { 
+                    e.target.src ="https://uxwing.com/wp-content/themes/uxwing/download/web-app-development/image-not-found-icon.png";
+     }}/>
+                 {/* <div>{item.id}</div> */}
+                    <div>{item.name}</div>
+                       <div className="tier" >Tier:{
+                       item.tier === 1 ? "I":
+                       item.tier === 2 ? "II":
+                       item.tier === 3 ? "III":
+                       item.tier === 4 ? "IV":
+                       item.tier === 5 ? "V": item.tear
+                       }</div> 
+          </div>
+        );
+      })
+     } </div>)
+}
+
+export default Consumable
+
+
+
+
