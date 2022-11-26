9d8b1effc7cb587b3a336436533d9ea8a37049d9
Stalius4 <deivydasrimgaila@gmail.com>
2022-11-26 21:06:38 +0000

    navbar pic added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import React, { useState, useEffect} from "react";
 import Consumable from './Items/Consumable/consumable';
 import Mote from './Items/Motes/motes';
+import NavBar from './Pages/NavBar/nav';
 
 function App() {
   const [loading, setLoading] = useState(false);
@@ -15,7 +16,7 @@ const [consumableList, setConsumableList] = useState ([])
   return (
     <div className="App">
    
-    
+    <NavBar></NavBar>
 <Consumable loading={loading} setLoading={setLoading}  consumableList={consumableList} setConsumableList={setConsumableList} ></Consumable>
 
 </div>
---
+++ src/Pages/NavBar/nav.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./nav.css"
+import  logo from "./NW-logo.svg"
+
+const NavBar = () => {
+
+
+    return(
+
+        <div className="outer-box">
+            <img src={logo} alt="logo"  className="nw-logo"/>
+            <div className="inner-box"></div> Navbar</div>
+    )
+}
+
+export default NavBar
