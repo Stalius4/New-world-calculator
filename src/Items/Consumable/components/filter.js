e4de3166e0ef96aa2057b0e4a343bba4a3b941bf
Stalius4 <deivydasrimgaila@gmail.com>
2022-11-26 19:28:01 +0000

    filter by finished

--- src/Items/Consumable/components/filter.js
+++ src/Items/Consumable/components/filter.js
@@ -4,44 +4,84 @@ import "./filter.css"
 
 const Filter = ({consumableList, setFiltConsumableList, setCurrentPage, setIconToggle}) => {
 
-const [filterBox, setFilterBox] = useState({main:false, rarity:false, tear:false})
+const [filterBox, setFilterBox] = useState({main:false, rarity:false, tier:false})
 const [rarityValue, setRarityValue] = useState([])
+const [tierValue, setTierValue] = useState([])
 
-
-  useEffect (() => {
-//if array is not empty 
-  if(rarityValue[0] >= 0){
-    //filter array with multiple values of array
-    const result = consumableList.filter(item => rarityValue.includes(item.rarity))
-    setFiltConsumableList(result);
-    setCurrentPage(1)
+  useEffect(() => {
+    //with rarity and tier options
+    if (rarityValue[0] >= 0 && tierValue[0] >= 0) {
+      //filter array with multiple values of array
+      const result = consumableList.filter((item) =>tierValue.includes(item.tier) && rarityValue.includes(item.rarity));
+      setFiltConsumableList(result);
+      setCurrentPage(1);
       //remove icon from name  when other sort button pressed
-      setIconToggle(0)
+      setIconToggle(0);
+    }
+
+
+    //with rarity option
+    if (rarityValue[0] >= 0 && tierValue[0] === undefined) {
+      //filter array with multiple values of array
+      const result = consumableList.filter((item) =>
+        rarityValue.includes(item.rarity)
+      );
+      setFiltConsumableList(result);
+      setCurrentPage(1);
+      //remove icon from name  when other sort button pressed
+      setIconToggle(0);
+    }
+
+
+    //with tier option
+    if (tierValue[0] >= 0 && rarityValue[0] === undefined) {
+      //filter array with multiple values of array
+      const result = consumableList.filter((item) =>
+        tierValue.includes(item.tier)
+      );
+      setFiltConsumableList(result);
+      setCurrentPage(1);
+      //remove icon from name  when other sort button pressed
+      setIconToggle(0);
     }
     //if array is empty display full lists of consumable items
-if(rarityValue[0] === undefined){
-    setFiltConsumableList(consumableList)
-    setIconToggle(0)
-    setCurrentPage(1)
-}
- 
-}, [rarityValue])
+    if (rarityValue[0] === undefined && tierValue[0] === undefined) {
+      setFiltConsumableList(consumableList);
+      setIconToggle(0);
+      setCurrentPage(1);
+    }
+  }, [rarityValue, tierValue]);
 
+
+
+  
 const rarityFilter = (e) => {
   if (e.target.checked) {
     setRarityValue((prev) => [...prev, Number(e.target.value)]);
   }
+
   if (!e.target.checked) {
-  
     //remove value from array
-    setRarityValue((prev) =>
-      prev.filter(function (v) {
+    setRarityValue((prev) =>prev.filter(function (v) {
         return v !== Number(e.target.value);
       })
     );
   }
 };
 
+const tierFilter = (e) => {
+    if (e.target.checked) {
+      setTierValue((prev) => [...prev, Number(e.target.value)]);
+    }
+  
+    if (!e.target.checked) {
+      //remove value from array
+      setTierValue((prev) =>prev.filter(function (v) {
+          return v !== Number(e.target.value);
+        })
+      );
+    }
+  };
 
     return(
     <>
@@ -77,7 +117,29 @@ const rarityFilter = (e) => {
 </label>
 </div>: ""}
 
-<div className="open-tear"> <div> Tear</div><i className={filterBox.tear  ?'fas fa-angle-up drop-icon' : 'fas fa-angle-down drop-icon '} ></i></div>
+<div className="open-tear" onClick={()=> {  setFilterBox(prevState => ({...prevState, tear: !prevState.tear}))}}> <div> Tear</div><i className={filterBox.tear  ?'fas fa-angle-up drop-icon' : 'fas fa-angle-down drop-icon '} ></i></div>{filterBox.tear ? <div className="open-rarity-options" >
+<label className="container ">I
+  <input type="checkbox" value={1}  onClick={(e)=>tierFilter(e)}></input>
+  <span className="checkmark"></span>
+
+</label>
+<label className="container ">II
+  <input type="checkbox" value={2} onChange={(e)=>tierFilter(e)}></input>
+  <span className="checkmark"></span>
+</label>
+<label className="container ">III
+  <input type="checkbox" value={3} onChange={(e)=>tierFilter(e)}></input>
+  <span className="checkmark"></span>
+</label>
+<label className="container">IV
+  <input type="checkbox"  value={4} onChange={(e)=>tierFilter(e)}></input>
+  <span className="checkmark"></span>
+</label>
+<label className="container ">V
+  <input type="checkbox"  value={5} onChange={(e)=>tierFilter(e)}></input>
+  <span className="checkmark"></span>
+</label>
+</div>: ""}
 </div> : ""}
 
 
