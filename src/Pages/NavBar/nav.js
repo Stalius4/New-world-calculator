e14ece9bd1b5f6000245c78827e286351edf793e
Stalius4 <deivydasrimgaila@gmail.com>
2022-11-27 20:13:04 +0000

    navbar style added

--- src/Pages/NavBar/nav.js
+++ src/Pages/NavBar/nav.js
@@ -5,23 +5,23 @@ import  logo from "./NW-logo.svg"
 
 const NavBar = () => {
 const [toggleDatabase, setToggleDatabase] = useState (false)
-const [toggleNavbar, setToggleNavbar] = useState(false)
+const [toggleNavbar, setToggleNavbar] = useState(true)
     return(
 
         <div className={toggleNavbar ? "outer-box" : "outer-box-mini"}>
             <button className={toggleNavbar ? "toggle-navbar-btn": "toggle-navbar-btn-mini"}onClick={() => setToggleNavbar(!toggleNavbar)}> <i className={toggleNavbar ? 'fas fa-angle-up ': 'fas fa-angle-down '}></i></button>
-            <div className="navbar">
-                <div className="inner-navbar">
- <ul >
-  <li className="home-li">Home</li>
-  <li className="database-li" onClick={() => setToggleDatabase(!toggleDatabase)}>Database <i className={toggleDatabase ?'fas fa-angle-up database-li' : 'fas fa-angle-down database-li '} ></i></li>
-  <li>Cooking</li>
-  <li>Engineering</li>
-</ul>
-</div>
-            </div>
+            <div className={toggleNavbar ? "navbar" : "navbar-mini"}>  </div>
+            <ul className={toggleNavbar ? "ul-nav" : "ul-nav-mini"} >
+                <li className="home-li">Home</li>
+                <li className="database-li" onClick={() => setToggleDatabase(!toggleDatabase)}>Database <i className={toggleDatabase ?'fas fa-angle-up database-li' : 'fas fa-angle-down database-li '} ></i>  <div className="database-open"></div></li>
+                <li>Cooking</li>
+                <li>Engineering</li>
+            </ul>
+
+      
             <img src={logo} alt="logo"  className={toggleNavbar ? "nw-logo": "nw-logo-mini"}/>
-            <div className="inner-box"></div></div>
+            <div className="inner-box"></div>
+          </div>
     )
 }
 
